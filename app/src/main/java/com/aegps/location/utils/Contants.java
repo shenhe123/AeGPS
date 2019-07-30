36351fc61e13d62183828225ff9203f157075a7c
ShenHe <1527360050@qq.com>
2019-07-30 18:07:51 +0800

    by shenhe keep alive

---
+++ app/src/main/java/com/aegps/location/utils/Contants.java
@@ -0,0 +1,12 @@
+package com.aegps.location.utils;
+
+/**全局常量
+ *
+ * Created by shenhe on 2019/7/30.
+ */
+
+public class Contants {
+    public static final boolean DEBUG = true;
+
+    public static final String PACKAGE_NAME = "com.jiangdg.keepappalive";
+}
