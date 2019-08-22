086d421fb0752b49100719be562cc5cc627938bb
Shenhe <1527360050@qq.com>
2019-08-22 18:44:20 +0800

    by shenhe 加入电池优化白名单

--- app/src/main/java/com/aegps/location/RemoteLoginActivity.java
+++ app/src/main/java/com/aegps/location/RemoteLoginActivity.java
@@ -1,6 +1,11 @@
 package com.aegps.location;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -83,6 +88,26 @@ public class RemoteLoginActivity extends BaseActivity implements View.OnClickLis
 //        mTvAccount.setOnClickListener(this);
         mTvCompanyName = ((TextView) findViewById(R.id.tv_car_id));
         mTvCompanyName.setOnClickListener(this);
+
+        ignoreBatteryOptimization(this);
+    }
+
+    /**
+     * 忽略电池优化
+     */
+    private void ignoreBatteryOptimization(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+
+            boolean hasIgnored = powerManager.isIgnoringBatteryOptimizations(activity.getPackageName());
+            //  判断当前APP是否有加入电池优化的白名单，如果没有，弹出加入电池优化的白名单的设置对话框。
+            if (!hasIgnored) {
+                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + activity.getPackageName()));
+                startActivity(intent);
+            }
+        }
     }
 
     public void attemptLogin(View view) {
